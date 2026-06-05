e667b1ebd48e796d2fe2b28bb63cb03d07c704eb
FredhKhelyan <fredhkamgaing@gmail.com>
2026-06-05 08:05:30 +0100

    modification

---
+++ Tableau.java
@@ -0,0 +1,10 @@
+public class Tableau {
+    public static void main(String[] args) {
+        int tableauNumero[] = { 1, 2, 3 };
+        
+        for (int i = 0; i < tableauNumero.length; i++) {
+            System.out.println("on a: " + tableauNumero[i]);
+
+        }
+    }
+}
