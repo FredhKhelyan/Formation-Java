01d829c9cb89df0e3c8fd61dbbec7e0407a5dd34
FredhKhelyan <fredhkamgaing@gmail.com>
2026-06-10 13:26:25 +0100

    +

--- GestionNotes.java
+++ GestionNotes.java
@@ -2,7 +2,7 @@
 // Projet de gestion de notes
 //  * Deamnder le nombre de note que le user veur saisir
 // 1- creer un tableau de type double
-// 2- remplir ce tableau par des notes et coefficients remplies par le user
+// 2- remplir ce tableau par des notesremplies par le user
 // 3 - Calcul de la moyenne, la note la plus haute et la plus basse
 // 4- Determine lw nombre de notes > 10
 import java.util.Scanner;
@@ -11,15 +11,13 @@ import java.util.Scanner;
 public class GestionNotes {
     public static void main(String[] args) {
         int nbreNote;
-        double notes[];
 
         System.out.print("Bienvenue cher professeur, veuillez donner le nombre de notes que vous souhaitez remplir: ");
         Scanner sc = new Scanner(System.in);
         nbreNote = sc.nextInt();
 
+        double notes[] = new double[nbreNote];
         
-
-
     }
 
 }
